2bd308daac0678970a92760dfbc65530a273bd82
Enrique-flr <77118423+Enrique-flr@users.noreply.github.com>
2021-11-04 11:24:56 -0600

    Initial commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  constructor(private authSvc: AuthService, private router:Router) {}
+
+  ngOnInit() {}
+
+  async onLogin(email, password) {
+    try {
+      const user = await this.authSvc.login(email.value, password.value);
+      if (user) {
+        const isVerified = this.authSvc.isEmailVerified(user);
+        console.log('verid', isVerified);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log('Error ', error);
+    }
+  }
+  //Login Google FALTA COMPLETAR LAS LIBRERIAS DE AUTENTIFICACION CON GOOGLE.
+  // async onLoginGoogle(){
+  //   try{
+  // const user = await this.authSvc.loginGoogle();
+  // if(user){
+    //const isVerified = this.authSvc.isEmailVerified(user);
+    //console.log('verid', isVerified);
+  // }
+  //   }catch(error){
+  //     console.log('Error ', error);
+  //   }
+  // }
+
+  private redirectUser(isVerified:boolean): void{
+    if(isVerified){
+      this.router.navigate(['admin']);
+    }
+    else{
+      this.router.navigate(['verify-email']);
+    }
+    }
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDX_7T4kbeyUSt7ixwNMpPv1s_T4WU4eDQ",
+    authDomain: "apprey-490c0.firebaseapp.com",
+    projectId: "apprey-490c0",
+    storageBucket: "apprey-490c0.appspot.com",
+    messagingSenderId: "366756511954",
+    appId: "1:366756511954:web:8bebb8ce21c421e5f72d0b",
+    measurementId: "G-2DHMN7PJ7Z"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
